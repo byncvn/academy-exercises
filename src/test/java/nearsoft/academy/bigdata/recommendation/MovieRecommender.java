3c930ee1633d0ea7fb825ec21116430de4a36e28
david.alpuche <alpuche1990@gmail.com>
2022-04-19 19:19:54 -0500

    Refactoring the Movie recommender class and adding comments on those changes

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -1,5 +1,21 @@
 package nearsoft.academy.bigdata.recommendation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
@@ -11,130 +27,88 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-import java.io.*;
-import java.util.*;
-
 public class MovieRecommender {
 
-    private ArrayList<Review> reviewList = new ArrayList<>();
-    private Set<String> products = new HashSet<String>();
-    private Set<String> users = new HashSet<String>();
+    private final String PRODUCT_ID_IDENTIFIER = "product/productId";
+    private final String USER_ID_IDENTIFIER = "review/userId";
+    private final String SCORE_IDENTIFIER = "review/score";
 
-    private HashMap<String, Integer> usersInts = new HashMap<String, Integer>();
+    private List<Review> reviewList = new ArrayList();
+    private int reviewCounter = 0;
+    private Map<String, Integer> usersMap = new HashMap<>();
+    private Map<String, Integer> productsMap = new HashMap<>();
+    private Map<Integer, String> productsIdMap = new HashMap<>();
 
-    private HashMap<String, Integer> productsInts = new HashMap<String, Integer>();
-    private int counterUser = 0;
-    private int counterProduct = 0;
-    private String currentProduct;
-    private String currentUser;
+    public MovieRecommender(String filepath)  {//no need for the extra throws words
+        File csvFile = new File("dataBase.csv");
 
-   // String score;
-
-    public MovieRecommender(String filepath ) throws FileNotFoundException, TasteException {
-
-
-        try{
-            FileReader fr = new FileReader(filepath);
-            BufferedReader br = new BufferedReader(fr);
+        //All the lines (46-48) are using resources that we need to close after the processing, this "try-with-resources" will handle that so we dont need to do it manually
+        try (FileReader fr = new FileReader(filepath);
+             BufferedReader br = new BufferedReader(fr);
+             PrintWriter out = new PrintWriter(csvFile)) {
 
             String newLine;
-
-            Review object = new Review();
-
-            File csvFile = new File("dataBase.csv");
-            PrintWriter out = new PrintWriter(csvFile);
-
+            Review review = new Review();
+            String currentProduct = "";
+            String currentUser = "";
             while ((newLine = br.readLine()) != null) {
-
-                String words[] = newLine.split(":");
-
-                if (words[0].equals("product/productId")) {
-
-                    reviewList.add(object);
-                    object = new Review();
-
-                    object.setProductId(words[1].trim());
-                    products.add(words[1].trim());
-
-                    if(!productsInts.containsKey(words[1].trim())) {
-                        productsInts.put(words[1].trim(), counterProduct);
-                        counterProduct++;
+                if (newLine.contains(":")) {//Using this to avoid splitting and processing empty lines
+                    String line[] = newLine.split(":");
+                    String trimmedValue = line[1].trim();
+                    switch (line[0]) {//Added a switch statement to avoid multiple ifs
+                        case PRODUCT_ID_IDENTIFIER:// added the identifier words as constant, for better readability
+                            //We created a new review if we found a ProductId identifier
+                            // reviewList.add(review);//This is an scalable solution if we want to process the reviews on future iteration but for now we only need the size
+                            reviewCounter++;
+                            review = new Review();
+                            review.setProductId(trimmedValue);
+                            currentProduct = trimmedValue;
+                            if(!productsMap.containsKey(currentProduct)){
+                                int index= productsMap.size();
+                                productsMap.put(currentProduct,index);
+                                productsIdMap.put(index,currentProduct);//created another map to store id-value structure for better processing
+                            }
+                            break;
+                        case USER_ID_IDENTIFIER:
+                            review.setUserId(trimmedValue);
+                            currentUser = trimmedValue;
+                            if(!usersMap.containsKey(currentUser)){
+                                int index= usersMap.size();
+                                usersMap.put(currentUser,index);
+                            }
+                            break;
+                        case SCORE_IDENTIFIER:
+                            review.setScore(trimmedValue);
+                            out.println(usersMap.get(currentUser)+ "," +productsMap.get(currentProduct) + "," + review.getScore());
+                            break;
                     }
-                    currentProduct = words[1].trim();
-
                 }
-
-               else if (words[0].equals("review/userId")) {
-
-                    object.setUserId(words[1].trim());
-
-                    users.add(words[1].trim());
-
-                    if(!usersInts.containsKey(words[1].trim())) {
-                        usersInts.put(words[1].trim(), counterUser);
-                        counterUser++;
-                    }
-                    currentUser = words[1].trim();
-
-
-                }
-                else if (words[0].equals("review/score")) {
-
-                    object.setScore(words[1].trim());
-
-                    out.println(usersInts.get(currentUser) + "," +productsInts.get(currentProduct) + "," + object.getScore());
-
-                }
-
             }
-            out.close();
-
-        }catch(Exception ex){
-            System.out.println("there is no file in the path");
+        } catch (Exception ex) {
+            System.out.println("There was an exception running the process:" + ex.getStackTrace());//Added a more clear error description
         }
     }
 
     public int getTotalReviews() {
-
-        return reviewList.size();
-
+        return reviewCounter;
     }
 
     public int getTotalProducts() {
-
-        return products.size();
-
+        return productsMap.size();
     }
 
     public int getTotalUsers() {
-
-        return users.size();
-
+        return usersMap.size();
     }
 
-    public List<String> getRecommendationsForUser(String userId) throws IOException, TasteException{
-
-        long userIntValue = usersInts.get(userId);
-        List<String> recommendsList = new ArrayList<String>();
-
+    public List<String> getRecommendationsForUser(String userId) throws IOException, TasteException {
         DataModel model = new FileDataModel(new File("dataBase.csv"));
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
         UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
         UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+        List<RecommendedItem> recommendations = recommender.recommend(usersMap.get(userId), 3);
 
-        List<RecommendedItem> recommendations = recommender.recommend(userIntValue, 3);
-        for (RecommendedItem recommendation : recommendations) {
-            recommendsList.add(getProductString((int)recommendation.getItemID()));
+        return recommendations.stream().map(r->productsIdMap.get((int)r.getItemID())).collect(Collectors.toList());//using Java 8 streams to search on the productIdMap
+    }
 
-        }
-        return recommendsList;
-    }
-    private String getProductString(int val) {
-        for(String key : this.productsInts.keySet()){
-            if(this.productsInts.get(key) == val){
-                return key;
-            }
-        }
-        return null;
-    }
 }
